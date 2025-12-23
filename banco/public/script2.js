405e041a8c050771352a4ce3a776e96081b1c05e
luiz11santista <luizuzumaki3009@gmail.com>
2025-12-23 18:20:13 -0300

    maaais front-end. Paginas de perfil, cartoes e transaçoes.

---
+++ banco/public/script2.js
@@ -0,0 +1,33 @@
+// let card = document.createElement('div')
+// card.className = 'card w-25 m-2'
+// let card2 = document.createElement('div')
+// card2.className = 'card-body'
+// let r = document.createElement('div')
+// r.className = 'row'
+// let column1 = document.createElement('div')
+// column1.className = 'col-6'
+// let column2 = document.createElement('div')
+// column2.className = 'col-6'
+// let imgcard = document.createElement('img')
+// imgcard.className = 'card-image w-100'
+// imgcard.src = 'credit-card.jpg'
+// let p1 = document.createElement('p')
+// p1.textContent = 'Número: '
+// p1.className = 'card-text'
+// let p2 = document.createElement('p')
+// p2.textContent = 'Saldo: '
+// p2.className = 'card-text'
+// let btn = document.createElement('button')
+// btn.className = 'btn btn-primary'
+// btn.textContent = 'Ver extrato'
+// let main_r = document.querySelector("#main")
+// main_r.appendChild(card)
+// card.appendChild(card2)
+// card2.appendChild(r)
+// r.appendChild(column1)
+// r.appendChild(column2)
+// column1.appendChild(imgcard)
+// column2.appendChild(p1)
+// column2.appendChild(p2)
+// column2.appendChild(btn)
+// criei isso dai em cima no console do google ta salvo p dps eu configurar
